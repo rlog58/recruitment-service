e22a7e93c44814d10d29bbc2d86ed71b20c9155a
Roman Logoida <Roman_Logoida@epam.com>
2021-02-20 01:45:52 +0300

    Create EmployeeService.java

---
+++ src/main/java/org/recruitmentservice/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package org.recruitmentservice.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.recruitmentservice.entity.Employee;
+import org.recruitmentservice.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public Employee getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new NoSuchEmployeeException(employeeId));
+        return employee;
+    }
+
+}
